def9ce69a8d60d4d4ac07bd6fa8b35ef9a7933a6
cristmasbox <sonne.zucker@gmx.de>
2026-08-11 13:25:07 +0200

    dependencies added

--- seshat/build.gradle.kts
+++ seshat/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     // custom libraries
     implementation(libs.maat)
     implementation(libs.glyphconverter)
+    implementation(libs.github.signprovider);
 
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
