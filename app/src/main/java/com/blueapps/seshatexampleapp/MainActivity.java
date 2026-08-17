136ad50bbcfd4e1aa8d899369c67b322cad51ba0
ThothDroid <website-tutorials@gmx.de>
2026-08-17 13:05:00 +0200

    implement SVG export functionality with SAF and update UI elements visibility

--- app/src/main/java/com/blueapps/seshatexampleapp/MainActivity.java
+++ app/src/main/java/com/blueapps/seshatexampleapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.blueapps.seshatexampleapp;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,13 +19,14 @@ import androidx.core.view.WindowInsetsCompat;
 import com.blueapps.seshat.Seshat;
 import com.blueapps.seshatexampleapp.databinding.ActivityMainBinding;
 
-import org.w3c.dom.Document;
+import java.io.IOException;
+import java.io.OutputStream;
 
-import javax.xml.transform.TransformerException;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityResultCallback<ActivityResult> {
 
     private ActivityMainBinding binding;
+
+    private String exportContent = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +40,54 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), this);
+
         binding.buttonExport.setOnClickListener(v -> {
             Seshat seshat = new Seshat(binding.input.getText().toString(), null, null);
-            binding.output.setText(seshat.convertToSVGString(this));
+            exportContent = seshat.convertToSVGString(this);
+            startSAF(activityResultLauncher);
         });
+    }
+
+    private void startSAF(ActivityResultLauncher<Intent> activityResultLauncher){
+
+        // Start SAF
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.setType("image/svg+xml");
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"image/svg+xml"});
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_TITLE, "test.svg");
+        activityResultLauncher.launch(intent);
+
+    }
+
+    public void writeFile(ContentResolver contentResolver, Uri uri, String content) throws IOException {
+        try (OutputStream outputStream = contentResolver.openOutputStream(uri)) {
+            if (outputStream != null) {
+                outputStream.write(content.getBytes());
+            } else {
+                throw new IOException("Unable to open OutputStream for URI: " + uri);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            // There are no request codes
+            Intent data = result.getData();
+            if (data != null) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    try {
+                        writeFile(getContentResolver(), uri, exportContent);
+                        exportContent = "";
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 }
