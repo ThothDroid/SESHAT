2e6fb80c2858921a1380c6b3feb8f96c0f68c4d0
ThothDroid <website-tutorials@gmx.de>
2026-09-10 20:20:31 +0200

    getter and setter for properties

--- seshat/src/main/java/com/blueapps/seshat/Seshat.java
+++ seshat/src/main/java/com/blueapps/seshat/Seshat.java
@@ -91,4 +91,110 @@ public class Seshat {
         }
     }
 
+    // Getters and Setters for properties
+
+
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(float textSize) {
+        this.textSize = textSize;
+    }
+
+    public int getVerticalOrientation() {
+        return verticalOrientation;
+    }
+
+    public void setVerticalOrientation(int verticalOrientation) {
+        this.verticalOrientation = verticalOrientation;
+    }
+
+    public int getWritingDirection() {
+        return writingDirection;
+    }
+
+    public void setWritingDirection(int writingDirection) {
+        this.writingDirection = writingDirection;
+    }
+
+    public int getWritingLayout() {
+        return writingLayout;
+    }
+
+    public void setWritingLayout(int writingLayout) {
+        this.writingLayout = writingLayout;
+    }
+
+    public boolean isDrawLines() {
+        return drawLines;
+    }
+
+    public void setDrawLines(boolean drawLines) {
+        this.drawLines = drawLines;
+    }
+
+    public float getLineThickness() {
+        return lineThickness;
+    }
+
+    public void setLineThickness(float lineThickness) {
+        this.lineThickness = lineThickness;
+    }
+
+    public float getPagePaddingLeft() {
+        return pagePaddingLeft;
+    }
+
+    public void setPagePaddingLeft(float pagePaddingLeft) {
+        this.pagePaddingLeft = pagePaddingLeft;
+    }
+
+    public float getPagePaddingTop() {
+        return pagePaddingTop;
+    }
+
+    public void setPagePaddingTop(float pagePaddingTop) {
+        this.pagePaddingTop = pagePaddingTop;
+    }
+
+    public float getPagePaddingRight() {
+        return pagePaddingRight;
+    }
+
+    public void setPagePaddingRight(float pagePaddingRight) {
+        this.pagePaddingRight = pagePaddingRight;
+    }
+
+    public float getPagePaddingBottom() {
+        return pagePaddingBottom;
+    }
+
+    public void setPagePaddingBottom(float pagePaddingBottom) {
+        this.pagePaddingBottom = pagePaddingBottom;
+    }
+
+    public float getSignPadding() {
+        return signPadding;
+    }
+
+    public void setSignPadding(float signPadding) {
+        this.signPadding = signPadding;
+    }
+
+    public float getLayoutSignPadding() {
+        return layoutSignPadding;
+    }
+
+    public void setLayoutSignPadding(float layoutSignPadding) {
+        this.layoutSignPadding = layoutSignPadding;
+    }
+
+    public float getInterLinePadding() {
+        return interLinePadding;
+    }
+
+    public void setInterLinePadding(float interLinePadding) {
+        this.interLinePadding = interLinePadding;
+    }
 }
