7638f8dc2313fc408632534dd571d6d143de1a3b
ThothDroid <website-tutorials@gmx.de>
2026-09-10 20:51:11 +0200

    properties added to ui logic

--- app/src/main/java/com/blueapps/seshatexampleapp/MainActivity.java
+++ app/src/main/java/com/blueapps/seshatexampleapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.blueapps.seshatexampleapp;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
@@ -27,6 +31,11 @@ public class MainActivity extends AppCompatActivity implements ActivityResultCal
     private ActivityMainBinding binding;
 
     private String exportContent = "";
+    private Seshat seshat;
+
+    private static final int[] Colors = {Color.BLACK, Color.WHITE, Color.GRAY, Color.CYAN, Color.MAGENTA, Color.YELLOW};
+    private int textColorCursor = 0;
+    private int bgColorCursor = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +52,235 @@ public class MainActivity extends AppCompatActivity implements ActivityResultCal
         ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(), this);
 
+        seshat = new Seshat(binding.input.getText().toString());
+
         binding.buttonExport.setOnClickListener(v -> {
-            Seshat seshat = new Seshat(binding.input.getText().toString());
             exportContent = seshat.convertToSVGString(this, "Test", "Test description");
             startSAF(activityResultLauncher);
+        });
+
+        // Define verticalOrientation RadioGroup
+        binding.VerticalOrientation.setOnCheckedChangeListener((radioGroup, i) -> {
+            if (i == R.id.top){
+                seshat.setVerticalOrientation(0);
+            } else if (i == R.id.middle){
+                seshat.setVerticalOrientation(1);
+            } else if (i == R.id.bottom){
+                seshat.setVerticalOrientation(2);
+            } else {
+                seshat.setVerticalOrientation(1);
+            }
+        });
+
+        // Define writingLayout RadioGroup
+        binding.WritingLayout.setOnCheckedChangeListener((radioGroup, i) -> {
+            if (i == R.id.lines){
+                seshat.setWritingLayout(0);
+            } else if (i == R.id.columns){
+                seshat.setWritingLayout(1);
+            } else {
+                seshat.setWritingLayout(0);
+            }
+        });
+
+        // Define writingDirection RadioGroup
+        binding.WritingDirection.setOnCheckedChangeListener((radioGroup, i) -> {
+            if (i == R.id.ltr){
+                seshat.setWritingDirection(0);
+            } else if (i == R.id.rtl){
+                seshat.setWritingDirection(1);
+            } else {
+                seshat.setWritingDirection(0);
+            }
+        });
+
+        // Define TextSize Seekbar
+        binding.textSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.textSizeTitle.setText(getString(R.string.text_size_title) + i + "px");
+                seshat.setTextSize(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // Define text color Button
+        binding.changeTextColor.setOnClickListener(view -> {
+            //seshat.setPrimarySignColor(Colors[textColorCursor]);
+            binding.changeTextColor.setBackgroundColor(Colors[textColorCursor]);
+            textColorCursor++;
+            if (textColorCursor >= Colors.length){
+                textColorCursor = 0;
+            }
+        });
+
+        // Define bg color Button
+        binding.changeBGColor.setOnClickListener(view -> {
+            //seshat.setBackgroundColor(Colors[bgColorCursor]);
+            binding.changeBGColor.setBackgroundColor(Colors[bgColorCursor]);
+            bgColorCursor++;
+            if (bgColorCursor >= Colors.length){
+                bgColorCursor = 0;
+            }
+        });
+
+        // Lines
+        binding.lineThickness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.lineThicknessTitle.setText(getString(R.string.line_thickness_title) + i + "px");
+                seshat.setLineThickness(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        binding.drawLines.setOnCheckedChangeListener((compoundButton, b) -> {
+            seshat.setDrawLines(b);
+        });
+
+        // Paddings
+        binding.paddingLeft.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.paddingLeftTitle.setText(getString(R.string.padding_left_title) + i + "px");
+                seshat.setPagePaddingLeft(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.paddingTop.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.paddingTopTitle.setText(getString(R.string.padding_top_title) + i + "px");
+                seshat.setPagePaddingTop(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.paddingRight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.paddingRightTitle.setText(getString(R.string.padding_right_title) + i + "px");
+                seshat.setPagePaddingRight(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.paddingBottom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.paddingBottomTitle.setText(getString(R.string.padding_bottom_title) + i + "px");
+                seshat.setPagePaddingBottom(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.signPadding.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.signPaddingTitle.setText(getString(R.string.sign_padding_title) + i + "px");
+                seshat.setSignPadding(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.layoutSignPadding.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.layoutSignPaddingTitle.setText(getString(R.string.layout_sign_padding_title) + i + "px");
+                seshat.setLayoutSignPadding(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.interLinePadding.setProgress(25);
+        binding.interLinePadding.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.interLinePaddingTitle.setText(getString(R.string.inter_line_padding) + i + "px");
+                seshat.setInterLinePadding(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
         });
     }
 
