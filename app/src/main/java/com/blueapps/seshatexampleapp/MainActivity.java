ff9cb7a1ee01aa08c0c67f08a7b01505a556292f
ThothDroid <website-tutorials@gmx.de>
2026-09-11 15:00:06 +0200

    remove unused items in svg

--- app/src/main/java/com/blueapps/seshatexampleapp/MainActivity.java
+++ app/src/main/java/com/blueapps/seshatexampleapp/MainActivity.java
@@ -95,24 +95,28 @@ public class MainActivity extends AppCompatActivity implements ActivityResultCal
                     binding.qualityTitle.setVisibility(View.GONE);
                     binding.quality.setVisibility(View.GONE);
                     binding.size.setVisibility(View.GONE);
+                    binding.autoSizeRatio.setVisibility(View.GONE);
                 } else if (i == 1){
                     binding.backgroundWithCSS.setVisibility(View.GONE);
                     binding.transparentBackground.setVisibility(View.VISIBLE);
                     binding.qualityTitle.setVisibility(View.VISIBLE);
                     binding.quality.setVisibility(View.VISIBLE);
                     binding.size.setVisibility(View.VISIBLE);
+                    binding.autoSizeRatio.setVisibility(View.VISIBLE);
                 } else if (i == 2){
                     binding.backgroundWithCSS.setVisibility(View.GONE);
                     binding.transparentBackground.setVisibility(View.GONE);
                     binding.qualityTitle.setVisibility(View.VISIBLE);
                     binding.quality.setVisibility(View.VISIBLE);
                     binding.size.setVisibility(View.VISIBLE);
+                    binding.autoSizeRatio.setVisibility(View.VISIBLE);
                 } else {
                     binding.backgroundWithCSS.setVisibility(View.VISIBLE);
                     binding.transparentBackground.setVisibility(View.VISIBLE);
                     binding.qualityTitle.setVisibility(View.GONE);
                     binding.quality.setVisibility(View.GONE);
                     binding.size.setVisibility(View.GONE);
+                    binding.autoSizeRatio.setVisibility(View.GONE);
                 }
             }
 
